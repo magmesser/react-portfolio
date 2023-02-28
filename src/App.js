43c4e948522d9605f2bb943c609fe0cbd6f50edf
magmesser <magmesser@gmail.com>
2023-02-28 17:22:51 -0500

    Added general styling and component layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import Portfolio from './components/Portfolio';
-import './App.css';
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
 
-export default function App() {
-  return <Portfolio />;
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+    </>
+  )
 }
+
+export default App
+
