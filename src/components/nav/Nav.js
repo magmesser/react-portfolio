d34b49924f0f6c35418de4b8ec0af436d0d109da
magmesser <magmesser@gmail.com>
2023-03-02 15:40:53 -0500

    Added nav bar

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Nav.css'
+import {BiHomeAlt} from 'react-icons/bi'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsPencilSquare} from 'react-icons/bs'
+import {BsCodeSquare} from 'react-icons/bs'
+import {BiMessageDetail} from 'react-icons/bi'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#top')
   return (
-    <div>Nav</div>
+    <nav>
+      {/* Home */}
+      <a href='#top' onClick={() => setActiveNav('#top')} className={activeNav === '#top' ? 'active' : ''} ><BiHomeAlt/></a> 
+      {/* About */}
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a> 
+      {/* Experience */}
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsPencilSquare/></a> 
+      {/* Portfolio */}
+      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsCodeSquare/></a> 
+      {/* Contact */}
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageDetail/></a> 
+    </nav>
   )
 }
 
