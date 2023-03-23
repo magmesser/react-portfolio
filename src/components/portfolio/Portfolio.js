d12934e75c5c3ccc0de9d2fd661606cf465a5d8d
magmesser <magmesser@gmail.com>
2023-03-23 16:58:09 -0400

    Added new project Georgia Evergreens

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -7,14 +7,15 @@ import P4 from "../../assets/portfolio/P4.jpg";
 import P5 from "../../assets/portfolio/P5.jpg";
 import P6 from "../../assets/portfolio/P6.jpg";
 import P7 from "../../assets/portfolio/P7.jpg";
+import P8 from "../../assets/portfolio/P8.jpg";
 
 const data = [
   {
     id: 1,
-    image: P1,
-    title: "A Night In",
-    github: "https://github.com/magmesser/a-night-in",
-    demo: "https://magmesser.github.io/a-night-in/",
+    image: P8,
+    title: "Georgia Evergreens",
+    github: "https://github.com/magmesser/georgia-evergreens",
+    demo: "https://georgia-evergreens.herokuapp.com/",
   },
   {
     id: 2,
@@ -25,34 +26,41 @@ const data = [
   },
   {
     id: 3,
+    image: P1,
+    title: "A Night In",
+    github: "https://github.com/magmesser/a-night-in",
+    demo: "https://magmesser.github.io/a-night-in/",
+  },
+  {
+    id: 4,
     image: P3,
     title: "The Tech Blog",
     github: "https://github.com/magmesser/tech-blog",
     demo: "https://messer-tech-blog.herokuapp.com/",
   },
   {
-    id: 4,
+    id: 5,
     image: P4,
     title: "Just Another Text Editor",
     github: "https://github.com/magmesser/text-editor",
     demo: "https://messer-text-editor.herokuapp.com/",
   },
   {
-    id: 5,
+    id: 6,
     image: P5,
     title: "Lord of The Employees",
     github: "https://github.com/magmesser/employee-tracker",
     demo: "https://drive.google.com/file/d/1y76_hulzSaqx4xzvBzC-JCHqLmHkWm3F/view",
   },
   {
-    id: 6,
+    id: 7,
     image: P6,
     title: "Work Day Scheduler",
     github: "https://github.com/magmesser/hourly-scheduler",
     demo: "https://magmesser.github.io/hourly-scheduler/",
   },
   {
-    id: 7,
+    id: 8,
     image: P7,
     title: "ReadMe Generator",
     github: "https://github.com/magmesser/readme-generator",
